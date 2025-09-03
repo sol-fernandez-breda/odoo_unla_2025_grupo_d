845b3f9fe2462bfe15c9417ca9a3c987e080b31a
Sol Fernandez Breda <solfernandezbreda@gmail.com>
2025-09-02 21:21:35 -0300

    Correcciones de la primera entrega

--- Odoo 18/real_estate/__manifest__.py
+++ Odoo 18/real_estate/__manifest__.py
@@ -7,10 +7,10 @@
         'base'
     ],
     'data': [
+        'security/real_estate_res_groups.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/real_estate_menuitem.xml',
-        'views/real_estate_res_groups.xml',
     ],
     'application': True,
 
