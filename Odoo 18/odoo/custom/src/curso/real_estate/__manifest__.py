6c416cfd01967efda365c9083a0906c0addd18ce
Maria Eugenia Lopez <71157100+Euge16@users.noreply.github.com>
2025-09-16 17:39:56 -0300

    feature: se realizaron los ejercicios del 18 al 41 de la Guía 1

--- Odoo 18/odoo/custom/src/curso/real_estate/__manifest__.py
+++ Odoo 18/odoo/custom/src/curso/real_estate/__manifest__.py
@@ -7,10 +7,13 @@
         'base'
     ],
     'data': [
+        'security/real_estate_res_groups.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/real_estate_menuitem.xml',
-        'views/real_estate_res_groups.xml',
     ],
     'application': True,
 
